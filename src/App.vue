b4b597ace63c04159e3467b1629e6af201316371
Taku Okawa <taku.okawa@gmail.com>
2020-05-05 19:48:03 +0900

    REST API

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Todos from './components/Todos'
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -20,32 +21,33 @@ export default {
   },
   data(){
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        },                
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        // eslint-disable-next-line no-unused-vars
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err))
+      
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
+      const { title, completed } = newTodo
+
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+        .then(res =>  this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err))
     }
+  },
+  created(){
+    console.log('initialized')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err))
   }
 }
 </script>
